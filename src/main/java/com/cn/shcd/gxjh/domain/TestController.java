059429be48a0ecf53d8b8b14ce9490a9ef5e1bbc
zhh <mrzhang1206@163.com>
2020-03-24 23:04:35 +0800

    1

--- src/main/java/com/cn/shcd/gxjh/domain/TestController.java
+++ src/main/java/com/cn/shcd/gxjh/domain/TestController.java
@@ -23,10 +23,10 @@ import java.util.Map;
 /**
  * Created by zhh on 2020/2/23.
  */
-
 @RestController
 @ApiOperation(value = "测试接口")
 public class TestController {
+
     @Autowired
     private AccountManager accountManager;
 
@@ -46,23 +46,15 @@ public class TestController {
     @ApiOperation(value = "测试登录服务", notes = "测试登录服务")
     public String testAuth(HttpServletRequest request) {
 
-//        JSONObject date = new JSONObject();
-//        date.put("name","张豪豪");
-//        String count = "date";
-//        if(!date.isEmpty()){
-//            return ReturnFormat.ReturnString("返回成功","200",date);
-//        }
-//        String name = response.getHeader("user_name");
+
         String name = request.getHeader("user_name");
         String pwd  = request.getHeader("pwd");
 
-//        System.out.println(name+","+pwd);
-//        登陆的验证用户名密码
+
         Account account = accountManager.createAccount();
         Map<String, String> paramMap = new HashMap<>();
 //      放入验证的信息
         paramMap.put(AccountMeta.UID_PARAM_NAME, account.getUid().toString());
-//        paramMap.put(AccountMeta.PASSWORD_PARAM_NAME, account.getUid().toString());
 //      生成token秘钥
         String tok = JwtUtils.sign(authConfig.getSecretKey(), authConfig.getTokenRefreshSpace(), paramMap);
         String token = CodingUtils.encryptAES(tok, authConfig.getTokenSecretKey());
